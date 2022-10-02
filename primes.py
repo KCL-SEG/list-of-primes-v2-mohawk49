9e9f2346031657447bc149896a8008ba90190d4c
Michael Seiranian <michael.seiranian@kcl.ac.uk>
2022-10-02 22:37:18 +0100

    Add protection

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if(number_of_primes<1):
+        raise ValueError("No numbers less than 1")
+        return
     list = []
+    count = 0
+    number = 2
+    while count < number_of_primes:
+        for n in range(2,number):
+            if(number % n == 0):
+                break
+        else:
+            count+=1
+            list.append(number)
+        number+=1
     return list
